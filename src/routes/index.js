8d311e3bd15412fd589b21f1099bb4866eb7a820
volodymyrtymets <volodymyrtymets@gmail.com>
2017-02-21 11:31:34 +0200

    add firs api route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import middleware from '../middleware';
 import initializeDb from '../db';
 import config from '../config';
+import restaurant from '../controller/restaurant'
 
 let router = express();
 
@@ -12,6 +13,7 @@ let router = express();
    router.use(middleware({ config, db }));
 
    // api routers v1(/v1)
+   router.use('/restaurant', restaurant({ config, db }));
  });
 
 export default router;
